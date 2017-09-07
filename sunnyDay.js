fe16febf2e33644cdb59e5bdb0ec1092258fe961
Jai <31632266+jaip777@users.noreply.github.com>
2017-09-07 11:20:22 -0400

    Challenge: 'It's A Beautiful Day'
    Challenge: 'It's A Beautiful Day' on KA - 'Drawing with Code'

---
+++ sunnyDay.js
@@ -0,0 +1,12 @@
+background (55, 115, 212);
+noStroke();
+fill(45, 240, 31);
+rect(0, 300, 400, 100);  // The grass
+fill(255, 230, 0);
+ellipse(80, 64, 100, 100);  // The sun
+
+// The cloud parts
+fill(255, 255, 255);
+ellipse(240, 90, 166, 127);
+ellipse(158, 100, 95, 77);
+ellipse(326, 100, 95, 77);
