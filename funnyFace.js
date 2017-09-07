ef9cc88118e37b1f79979a3566703ebdc2a7e348
Jai <31632266+jaip777@users.noreply.github.com>
2017-09-07 11:15:58 -0400

    Challenge: Funny Face
    Challenge: Funny Face in K.A - Intro To Drawing

---
+++ funnyFace.js
@@ -0,0 +1,7 @@
+
+ellipse(200, 180, 188, 260);  // Makes the face
+
+ellipse(160, 170, 60, 60);  // Makes the eye
+ellipse(235, 170, 30, 60);  // Makes the other eye
+
+ellipse(195, 245, 120, 40);  // Makes the mouth
