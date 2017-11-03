a9f0342af10c4da9b93d5ecc63e58b9a8509cc9e
Jai <31632266+jaip777@users.noreply.github.com>
2017-11-03 11:25:10 -0400

    Create Challenge:SunnySnowday.js

---
+++ Challenge:SunnySnowday.js
@@ -0,0 +1,13 @@
+background(0, 47, 255);
+fill(65, 196, 76);
+// The ground
+rect(0, 300, 400, 100);  
+fill(255, 230, 0);
+// The sun
+ellipse(80, 64, 100, 100);  
+fill(252, 252, 252);
+// The snowman
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
+
